3c6d54f0c78cfbbd70d151e18b8144f78fb4b0fe
Craig <craigjope@gmail.com>
2021-12-08 11:52:40 -0700

    index.js routing

--- src/Groups.js
+++ src/Groups.js
@@ -1,35 +1,18 @@
 import { Button } from "react-bootstrap";
-import { useState } from "react/cjs/react.development";
 import CreateGroup from "./CreateGroup";
 
-function Groups({ groups, onAddGroup }) {
-  const [activeGroupID, setActiveGroupID] = useState(2);
-
+function Groups({ groups, setActiveGroup, onAddGroup }) {
   const groupButtons = groups.map((group) => (
-    <Button
-      key={group.id}
-      value={group.id}
-      onClick={(e) => {
-        setActiveGroupID(e.target.value);
-      }}
-    >
-      {group.name} {group.emoji}
+    <Button onClick={(e) => setActiveGroup(group)} key={group.id}>
+      {group.name}
+      {group.emoji}
     </Button>
   ));
-
-  const group1 = groups
-    .filter((group) => group.id === activeGroupID)
-    .map((group) => group.name)
-    .toString(); //has this always been this difficult?!?!?
-
-  console.log(group1);
-  // console.log(activeGroupName);
 
   return (
     <div className="groups">
       {groupButtons}
       <CreateGroup onAddGroup={onAddGroup} />
-      {/* {activeGroupName} */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import AppBackup from "./AppBackup";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppBackup />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
